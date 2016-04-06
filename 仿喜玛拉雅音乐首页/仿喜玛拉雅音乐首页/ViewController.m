78300e1d68cb202d640c40f42335ce1d50f94848
susu <28025920@qq.com>
2016-04-06 16:03:03 +0800

    创建项目，添加素材

---
+++ 仿喜玛拉雅音乐首页/仿喜玛拉雅音乐首页/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  仿喜玛拉雅音乐首页
+//
+//  Created by  江苏 on 16/4/6.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
